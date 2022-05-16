72ec8e71de4b53a4435d791fa1000ae7cdbd043a
programer1983 <programer972@gmail.com>
2022-05-16 13:12:42 +0300

    adding styles;

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,12 @@
 
 
 const Header = () => {
+  const headrStyle = {
+    backgroundColor: "royalblue",
+    color: '#fff',
+  }
   return (
-    <header>
+    <header style={headrStyle}>
       <h1>Groceries List</h1>
     </header>
   )
