61f9d120a36cfdb9c4bfab6bf102ebacd329ebe9
programer1983 <programer972@gmail.com>
2022-05-16 13:01:45 +0300

    footer;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './Header';
 import Content from './Content';
+import Footer from './Footer';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
      <Header />
      <Content />
+     <Footer />
     </div>
   );
 }
